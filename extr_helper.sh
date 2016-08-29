43fdcdc27bc88a68a5b40bff8e33705bd4d23f68
Christoph "criztovyl" Schulz <ch.schulz@joinout.de>
2016-08-29 20:36:16 +0200

    Update extr_helper.sh

--- extr_helper.sh
+++ extr_helper.sh
@@ -43,6 +43,11 @@ XLS_DIR="$SRC_DIR/xls"
 PDF_DIR="$SRC_DIR/pdf"
 JSON_DIR="$CONV_DIR/json"
 
+# If those constants are not set, tries to read from environment EXTR_HELPER_(NAME).
+TABULA="tabula"
+LOFFICE="loffice"
+MOODLE_DL_USER=
+
 EXIT_OK=0
 EXIT_HELP=6
 EXIT_NO_SUCH_FILE=1
@@ -54,6 +59,7 @@ EXIT_AUSBPLAN_ALREADY_THERE=5
 # 2 - Functions
 
 display_usage() { echo "Usage: $0 extractor file [force]"; exit $EXIT_HELP; }
+
 already_there_msg()
 {
     # Args: exit_code
@@ -97,7 +103,26 @@ replace_suffix()
     echo ${1/%.${1##*.}/$2}
 }
 
+check_file()
+{
+    # Args: file mimeExpected name force
+    local file=$1
+    local mime=$2
+    local name=$3
+    local force=$4
+
+    [[ `file --mime-type -b "$file"` == "$mime" ]] || [ $force ] || { echo -e "Moodle expects a $mime file.\nPlease use \"force\" as third argument to force extraction." >&2; return 1; }
+}
+
+file_existance_check()
+{
+    [ ! -f "$Src" ] && { echo "No such file \"$Src\"!"; exit $EXIT_NO_SUCH_FILE; }
+}
+
 # 3 - CLI
+
+# Help
+[[ "$0" =~ ^-{1,2}h(elp)?$ ]] || [ -z "$1" ] || [ -z "$2" ] && { display_usage; }
 
 Action=$1
 Src=$2
@@ -106,12 +131,11 @@ Third=$3
 [ "$Third" == "force" ] && Force=1 || Force=
 [ "$Third" == "overwrite" ] && Overwrite=1 || Overwrite=
 
-# Help
-[[ "$0" =~ ^-{1,2}h(elp)?$ ]] || [ -z "$1" ] || [ -z "$2" ] && { display_usage; }
+[ -z "$TABULA" ] && [[ "$EXTR_HELPER_TABULA" ]] && TABULA=$EXTR_HELPER_TABULA
+[ -z "$LOFFICE" ] && [[ "$EXTR_HELPER_LOFFICE" ]] && LOFFICE=$EXTR_HELPER_LOFFICE
+[ -z "$MOODLE_DL_USER" ] && [[ "$EXTR_HELPER_MOODLE_DL_USER" ]] && MOODLE_DL_USER=$EXTR_HELPER_MOODLE_DL_USER
 
 mkdir -p $DATA_DIR $HTML_DIR $XLS_DIR $PDF_DIR $JSON_DIR
-
-[ ! -f "$2" ] && { echo "No such file \"$2\"!"; exit $EXIT_NO_SUCH_FILE; }
 
 Src_Name=`basename "$Src"`
 New_Src_Name=`date +%Y-%m-%d-%s`_$Src_Name
@@ -120,9 +144,22 @@ LO_Tmpdir=`mktemp -d /tmp/libreoffice-XXXXXXXXXXXX`
 trap "rm -rf $LO_Tempdir" EXIT INT
 
 case "$Action" in
+    moodle-dl)
+
+        Cookie_File=`mktemp`
+        Download_Target=`mktemp -d`
+
+        [[ "$MOODLE_DL_USER" ]] || { echo "Missing download user. Is EXTR_HELPER_MOODLE_DL_USER set?" >&2; exit $EXIT_MISSING_DL_USER; }
+
+        curl -d username=$MOODLE_DL_USER -d password=`read -sp "Moodle Password for $MOODLE_DL_USER: " && echo $REPLY` http://siemens.lernvision.de/login/index.php $Src --cookie-jar "$Cookie_File" -o /dev/null -o "$Download_Target/$Src_Name"
+
+        bash $0 moodle "$Download_Target/$Src_Name"
+        ;;
     moodle)
 
-        [[ "$Src" =~ ^.*\.xlsx?$ ]] || [ $Force ] || { echo -e "Moodle expects a .xls(x) file.\nPlease use \"force\" as third argument to force extraction." >&2; exit $EXIT_MOODLE_NON_XLS; }
+        file_existance_check
+        check_file $Src "application/vnd.ms-excel" "Moodle" $Force || exit $EXIT_MOODLE_NON_XLS
+
 
         parse_moodle_xls()
         {
@@ -130,7 +167,7 @@ case "$Action" in
             xls_file=$2
 
             # https://ask.libreoffice.org/en/question/1686/how-to-not-connect-to-a-running-instance/
-            loffice "-env:UserInstallation=file://$LO_Tmpdir" --convert-to html "$xls_file" --outdir "$HTML_DIR"
+            ${LOFFICE:-loffice} "-env:UserInstallation=file://$LO_Tmpdir" --convert-to html "$xls_file" --outdir "$HTML_DIR"
 
             ( cd "$HTML_DIR" && ln -fs "`basename "$(replace_suffix "$xls_file" .html)"`" "$(replace_suffix "$src_name" .html)"; )
         }
@@ -139,9 +176,10 @@ case "$Action" in
         ;;
     ausbplan|ausb_plan|ausbildungsplan)
 
-        [[ "$Src" =~ ^.*\.pdf$ ]] || [ $Force ] || { echo -e "ausb_plan expects a .pdf file.\nPlease use \"force\" as third argument to force extraction." >&2; exit $EXIT_AUSBPLAN_NON_PDF; }
+        file_existance_check
+        check_file $Src "application/pdf" "Ausbildungsplan" $Force || exit $EXIT_AUSBPLAN_NON_PDF
 
-        pdf_file=
+        pdf_file= # TODO Is this still required?
 
         parse_ausbplan_pdf()
         {
@@ -151,10 +189,16 @@ case "$Action" in
             json_file=$JSON_DIR/$(replace_suffix "$New_Src_Name" .json)
 
             # https://github.com/tabulapdf/tabula-java (Pure Java) or https://github.com/tabulapdf/tabula-extractor (JRuby)
-            # -rf is not --recursive --force, it's --spreadsheet --format ;)
-            tabula -ruf JSON "$pdf_file" > "$json_file"
+            # -r -> --spreadsheet (it's a spreadsheet)
+            # -f -> --format (the output format, JSON for us)
+            # -u -> --use-line-returns (tabula ignores line returns by default)
+            ${TABULA:-tabula} -ruf JSON "$pdf_file" > "$json_file"
 
-            # tabula seems to produce invalid JSON. Make it valid.
+            # tabula seems to produce invalid JSON, each row is interpreted as seperate spreadsheet with an single row as
+            # an [[element, element, element]] array, but they are not seperated by an ",", so there is [[...]][[...]][[...]]...)
+            # TODO Check if only ][ or also ][][
+
+            # Make Valid: Replace ]][[ with ],[
             sed 's/\]\]\[\[/\],\[/g' -i "$json_file"
 
             ( cd "$JSON_DIR" && ln -fs "`basename "$json_file"`" "$(replace_suffix "$src_name" .json)" )
