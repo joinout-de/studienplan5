ed2dfd9d3f04f079f41f0bef3c0b1f1692589a2d
Shoko19961 <Davring@live.de>
2016-08-31 22:55:55 +0200

    Update for extractor_ausbildungsplan
    This commit updates the extractor_ausbildungsplan.rb accordingly to the
    UML

--- extractor_ausbildungsplan.rb
+++ extractor_ausbildungsplan.rb
@@ -2,10 +2,19 @@ require "json"
 require "./structs.rb"
 require "Date"
 
-def extract(datei)
-	planElemente=[]
-	File.open(datei, "rb") do |f|
-		inhalt = JSON.parse(f.readlines.join(?\n))
+class ExtractorAusbildungsplan
+
+	attr_reader :data
+
+	def initialize(file)
+		@file = file
+		@data = nil
+	end
+
+	def extract()
+		klasse = Clazz.new("FS151","BSc","FST","ABB2015")
+		planElemente=[]
+		inhalt = JSON.parse(@file.readlines.join(?\n))
 		inhalt.shift
 		woche=""
 		zeitraum=""
@@ -21,15 +30,16 @@ def extract(datei)
 					taetigkeit = inhalt[zx4+3][spalte]["text"]
 				end
 				next if woche.empty?
-				puts woche
-				puts zeitraum
-				puts freieTage
-				puts taetigkeit
-				puts ""
-				puts ""
-				planElemente.push(Struct::PlanElement::FullWeek(taetigkeit,"FS151",nil,DateTime.strptime("1 "+woche+" 20"+zeitraum[-2,2],"%u KW %W %Y"),freieTage))
+				#puts woche
+				#puts zeitraum
+				#puts freieTage
+				#puts taetigkeit
+				#puts ""
+				#puts ""
+				planElemente.push(Struct::PlanElement::FullWeek(taetigkeit,klasse,nil,DateTime.strptime("1 "+woche+" 20"+zeitraum[-2,2],"%u KW %W %Y"),freieTage))
 			end
 		end
+		@data={klasse => planElemente}
 	end
-	planElemente
+
 end
